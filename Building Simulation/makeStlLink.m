0c649c390fe8cea2eceb7a20b04553b87254c982
geodefeffrey <64029523+geodefeffrey@users.noreply.github.com>
2020-05-01 15:42:10 -0500

    Createxd prototype app

--- Building Simulation/makeStlLink.m
+++ Building Simulation/makeStlLink.m
@@ -1,15 +1,8 @@
 function p = makeStlLink(stlfilename, parentAxes, faceColor)
 
-%don't need if all inputs are used
-% if (nargin == 1)
-%     parentAxes = gca;
-%     faceColor = [.7 .7 .7];
-% elseif (nargin == 2)
-%     faceColor = [.7 .7 .7];
-% end
 set(parentAxes, 'DataAspectRatio', [1 1 1]);
 stlTr = stlread(stlfilename);
-linkVerts = stlTr.Points; 
+linkVerts = stlTr.Points;
 linkFaces = stlTr.ConnectivityList;
 p = patch('Parent', parentAxes, 'Faces',linkFaces,'Vertices',linkVerts,'FaceColor',faceColor);
 
