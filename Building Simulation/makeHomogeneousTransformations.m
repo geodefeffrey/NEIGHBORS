5f1459fcbb917b7a69ed32ca4fc44b697fb0b9ca
geodefeffrey <64029523+geodefeffrey@users.noreply.github.com>
2020-08-17 14:08:57 -0500

    Updates to the building simulation collision detection

--- Building Simulation/makeHomogeneousTransformations.m
+++ Building Simulation/makeHomogeneousTransformations.m
@@ -1,10 +1,28 @@
-function [A1, A2, A3, A4, A5] = makeHomogeneousTransformations(degreesTheta1, degreesTheta2, degreesTheta3, degreesTheta4, degreesTheta5)
+function [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12] = makeHomogeneousTransformations(degreesTheta1, degreesTheta2, degreesTheta3, degreesTheta4, degreesTheta5,degreesTheta6,degreesTheta7,degreesTheta8 )
 %MAKEHOMOGENEOUSTRANSFORMATIONS Create the DH matrices for the arm.
 
-A1 = homogeneousTransformation(0, 0, 0, degreesTheta1);
-A2 = homogeneousTransformation(0, 0, 0, degreesTheta2);
-A3 = homogeneousTransformation(10000, 0, 0, degreesTheta3);
-A4 = homogeneousTransformation(10000,0, 0, degreesTheta4);
-A5 = homogeneousTransformation(0, 7.75, 0, degreesTheta5);
+stl_factor = 6233/247;%6233/250;          %scaling factor on buildings due to STL import. Improved STL may remove the need for this. This is set to be as close as collision detection will allow
+dist = 436.83;                  %the distance magnitude away from the origin
 
+A1 = homogeneousTransformation(-436.83*stl_factor, 0, 0, -33.8);
+A2 = homogeneousTransformation(0, 0, 0, degreesTheta1+33.8);
+
+
+A3 = homogeneousTransformation(0, 0, 0, degreesTheta2+33.8);
+
+A4 = homogeneousTransformation(-436.83*stl_factor,0, 0, 33.8);
+A5 = homogeneousTransformation(0, 0, 0, degreesTheta3-33.8);
+A6 = homogeneousTransformation(0, 0, 0, degreesTheta4-33.8);
+
+%2nd building
+
+A7 = homogeneousTransformation(436.83*stl_factor, 0, 0, 33.8);
+A8 = homogeneousTransformation(0, 0, 0, degreesTheta5-33.8);
+
+
+A9 = homogeneousTransformation(0, 0, 0, degreesTheta6-33.8);
+
+A10 = homogeneousTransformation(436.83*stl_factor,0, 0, -33.8);
+A11 = homogeneousTransformation(0, 0, 0, degreesTheta7+33.8);
+A12 = homogeneousTransformation(0, 0, 0, degreesTheta8+33.8);
 end
--- Building Simulation/testSTLimport.m
+++ Building Simulation/testSTLimport.m
@@ -1,6 +1,17 @@
 clear all; close all; clc
 
-link0 = makeStlLink('STL/Theater_section.STL',gca,[1 0.5 0])
+% link0 = makeStlLink('STL/Theater_section.STL',gca,[1 0.5 0])
 % link1 = makeStlLink('STL/SCARY Robot - Link1-2.Stl',gca,[1 0 0])
 % link2 = makeStlLink('STL/SCARY Robot - Link2-2.Stl',gca,[0 0 1])
 % link3 = makeStlLink('STL/SCARY Robot - Link3-2.Stl',gca,[1 1 0])
+
+% link0 = makeStlLink('STL/section1.STL',gca,[1 0.5 0])
+
+% link0 = makeStlLink('STL/Theater_section.STL',gca,[1 0.5 0])
+link1 = makeStlLink('STL/Theater_section_no_collor.STL',gca,[0 0.5 1])
+link1 = makeStlLink('STL/Bench_section_nocollor.STL',gca,[1 0.5 1])
+
+
+xlabel ("x")
+ylabel ("y")
+zlabel("z")
